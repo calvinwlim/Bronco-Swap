b75f3e4a5cdd639a2d769e1d6c9d4a0cc30bb534
jalenpaige <117240799+jalenpaige@users.noreply.github.com>
2024-02-17 14:30:32 -0800

    fix small error in spacing

--- Bronco-Swap/src/router/index.js
+++ Bronco-Swap/src/router/index.js
@@ -21,12 +21,7 @@ const router = createRouter({
     {
       path: '/create',
       name: 'create',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/CreateView.vue')
-
-      component: () => import('../views/AboutView.vue')
     },
     {
       path: '/sign-up',
