7368a68b3e9dd2ba2c91025d9c79b84d707ccd33
Jonathan Preiss <jp9232004@gmail.com>
2024-02-17 12:03:32 -0800

    started navbar

--- Bronco-Swap/src/App.vue
+++ Bronco-Swap/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
