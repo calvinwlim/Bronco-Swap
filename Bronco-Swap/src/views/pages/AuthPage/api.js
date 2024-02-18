1526fe629edf4fb7a46b68e4bbdc08c122ded3da
Jonathan Preiss <jp9232004@gmail.com>
2024-02-18 02:38:01 -0800

    fixed api keys

--- Bronco-Swap/src/views/pages/AuthPage/api.js
+++ Bronco-Swap/src/views/pages/AuthPage/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { VITE_CHAT_ENGINE_PROJECT_ID, VITE_CHAT_ENGINE_PRIVATE_KEY } from './keys';
 
 const loginRest = async (username, secret) => {
   return await axios.get("https://api.chatengine.io/users/me/", {
     headers: {
-      "Project-ID": import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID,
+      "Project-ID": VITE_CHAT_ENGINE_PROJECT_ID,
       "User-Name": username,
       "User-Secret": secret,
     },
@@ -14,7 +15,7 @@ const signupRest = async (username, secret) => {
   return await axios.post(
     "https://api.chatengine.io/users/",
     { username, secret },
-    { headers: { "Private-Key": import.meta.env.VITE_CHAT_ENGINE_PRIVATE_KEY } }
+    { headers: { "Private-Key": VITE_CHAT_ENGINE_PRIVATE_KEY } }
   );
 };
 // const signupRest = async (username, secret, email, first_name, last_name) => {
