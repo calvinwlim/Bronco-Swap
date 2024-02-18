f94c7f131711fd03436ab09a5905f567dc778c8b
pengoof <calvinwlim@yahoo.com>
2024-02-17 17:03:10 -0800

    p

--- Bronco-Swap/src/components/LandingPage.vue
+++ Bronco-Swap/src/components/LandingPage.vue
@@ -9,8 +9,6 @@
       Within Your Campus
       Community
     </h1>
-    <div class="blurry-gradient-1"></div>
-    <div class="blurry-gradient-2"></div>
   </div>
 </template>
 
@@ -20,32 +18,6 @@ h1 {
   font-size: 3rem;
   position: relative;
   top: -10px;
-}
-
-.blurry-gradient-1 {
-  position: fixed;
-  top: 100px;
-  left: 1000px;
-  transform: translate(-50%, 0%);
-  width: 300px;
-  height: 500px;
-	border-radius: 50% 22% 40% 80%;  
-	filter: blur(100px);
-  background: radial-gradient(circle at 50% 50%, #FAEF5D, #FF004D);
-  opacity: 0.2;
-}
-
-.blurry-gradient-2 {
-  position: fixed;
-  top: -700px;
-  left: 250px;
-  transform: translate(-50%, 0%);
-  width: 1200px;
-  height: 900px;
-	border-radius: 50% 22% 40% 80%;  
-	filter: blur(100px);
-  background: radial-gradient(circle at 50% 50%, #FAEF5D, #FF004D);
-  opacity: 0.2;
 }
 
 h3 {
