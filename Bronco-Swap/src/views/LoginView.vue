d971c048f52af75463e072effc2042b97cba1a55
Jonathan Preiss <jp9232004@gmail.com>
2024-02-18 03:55:36 -0800

    fiddle w reload

--- Bronco-Swap/src/views/LoginView.vue
+++ Bronco-Swap/src/views/LoginView.vue
@@ -34,7 +34,6 @@ export default {
               this.user = result.user;
               localStorage.setItem('user', JSON.stringify(this.user))
               this.$router.push("/");
-              window.location.reload();
           })
           .catch((error) => {
               console.error(error);
