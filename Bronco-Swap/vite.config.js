62facc6b6db38d62372733ab3506f7bc43a64567
jalenpaige <117240799+jalenpaige@users.noreply.github.com>
2024-02-19 19:57:43 -0800

    initial firebase hosting stuff

--- Bronco-Swap/vite.config.js
+++ Bronco-Swap/vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Bronco-Swap/",
+  base: "",
   css: {
     devSourcemap: true,
   },
